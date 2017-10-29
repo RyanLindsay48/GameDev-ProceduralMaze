6d410ff4cd3ac9ef9c2906b6a78e5964330949e6
Ryan Lindsay <lindsa48@students.rowan.edu>
2017-10-29 19:46:52 -0400

    Updated player movement
    Player is now able to rotate camera with mouse, and move position with WASD or arrows

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,50 +1,76 @@
 ﻿using UnityEngine;
 
+public enum Direction {
+	Forward,
+	Backward,
+	Left,
+	Right
+}
+
 public class Player : MonoBehaviour {
+	public float movementSpeed;
+	public float rotationSpeed;
 	private MazeCell currentCell;
-	private MazeDirection currentDirection;
+	private GameObject player;
+	private GameObject playerCamera;
+	private Vector3 currentDirection;
 
 
 
 	public void SetLocation (MazeCell cell) {
+		player = GameObject.FindGameObjectWithTag("Player");
+		playerCamera = GameObject.FindGameObjectWithTag("PlayerCamera");
+
 		if (currentCell != null) {
 			currentCell.OnPlayerExited();
 		}
 		currentCell = cell;
 		transform.localPosition = cell.transform.localPosition;
 		currentCell.OnPlayerEntered();
+
+		player.transform.position = cell.transform.position;
 	}
 
-	private void Move (MazeDirection direction) {
-		MazeCellEdge edge = currentCell.GetEdge(direction);
-		if (edge is MazePassage) {
-			SetLocation(edge.otherCell);
+	private void FixedUpdate () {
+		if (Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.UpArrow)) {
+			Move(Direction.Forward);
+		} 
+		else if (Input.GetKey (KeyCode.S) || Input.GetKey (KeyCode.DownArrow)) {
+			Move(Direction.Backward);
+		} 
+		else if (Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow)) {
+			Move(Direction.Left);
+		}
+		else if (Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.RightArrow)) {
+			Move(Direction.Right);
+		}
+		else if (Input.GetAxis("Mouse X") != 0f) {
+			Look(Input.GetAxis("Mouse X"));
 		}
 	}
 
-	private void Look (MazeDirection direction) {
-		transform.localRotation = direction.ToRotation();
-		currentDirection = direction;
+	private void Move (Direction direction) {
+		if (direction == Direction.Forward) {
+			currentDirection = playerCamera.transform.forward;
+		} 
+		else if (direction == Direction.Backward) {
+			currentDirection = -playerCamera.transform.forward;
+		} 
+		else if (direction == Direction.Left) {
+			currentDirection = -playerCamera.transform.right;
+		}
+		else { // (direction == Direction.Right) 
+			currentDirection = playerCamera.transform.right;
+		}
+
+		currentDirection.y = transform.position.y;
+		currentDirection.Normalize();
+		player.GetComponent<Rigidbody>().MovePosition(player.transform.position + currentDirection * movementSpeed * Time.deltaTime);
+
+		//updateLocation
 	}
 
-	private void Update () {
-		if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) {
-			Move(currentDirection);
-		}
-		else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) {
-			Move(currentDirection.GetNextClockwise());
-		}
-		else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) {
-			Move(currentDirection.GetOpposite());
-		}
-		else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) {
-			Move(currentDirection.GetNextCounterclockwise());
-		}
-		else if (Input.GetKeyDown(KeyCode.Q)) {
-			Look(currentDirection.GetNextCounterclockwise());
-		}
-		else if (Input.GetKeyDown(KeyCode.E)) {
-			Look(currentDirection.GetNextClockwise());
-		}
+	private void Look (float rotation) {
+		player.transform.Rotate(0, rotation * Time.deltaTime * rotationSpeed, 0);		
 	}
 }
